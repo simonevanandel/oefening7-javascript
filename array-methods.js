5b7d51998a28dd97194f8ab3102d15cf31737408
simonevanandel <simonevanandel@hotmail.com>
2023-08-23 22:11:27 +0200

    Huiswerk gemaakt

--- array-methods.js
+++ array-methods.js
@@ -15,7 +15,8 @@ console.log(cookieIngredients);
 
 const pizzas = ["Hawai", "Margherita"];
 
-
+pizzas.push('Salami');
+console.log(pizzas);
 
 /* Opdracht 2: */
 // Verwijder het éérste item van onderstaande array en log dit in de terminal
@@ -24,6 +25,9 @@ const pizzas = ["Hawai", "Margherita"];
 
 const brownieIngredients = ["Noten", "Eieren", "Bloem", "Cacao", "Chocoladestukjes", "Bakpoeder"];
 
+const firstItem = brownieIngredients.shift();
+console.log(firstItem);
+console.log(brownieIngredients);
 
 /* Opdracht 3: */
 // Draai de volgorde van onderstaande array met namen om en log de gehele array in de terminal om te kijken is het gelukt is
@@ -31,6 +35,8 @@ const brownieIngredients = ["Noten", "Eieren", "Bloem", "Cacao", "Chocoladestukj
 
 const names = ["Henk", "Jan", "Piet"];
 
+names.reverse();
+console.log(names);
 
 
 /* Opdracht 4: */
@@ -39,5 +45,6 @@ const names = ["Henk", "Jan", "Piet"];
 
 const cities = ["Utrecht", "Maastricht", "Eindhoven"];
 
-
+cities.splice(1,0,'Amsterdam');
+console.log(cities);
 
